1ffd0883e56c612f1bb7865822e125f724052681
jandsnadell <64447833+jandsnadell@users.noreply.github.com>
2026-06-06 06:26:47 -0700

    Create eliminatevirus.bat

---
+++ eliminatevirus.bat
@@ -0,0 +1,24 @@
+@echo off
+:: Check for permissions
+>nul 2>&1 "%SYSTEMROOT%\system32\cacls.exe" "%SYSTEMROOT%\system32\config\system"
+
+:: If error flag set, we do not have admin.
+if '%errorlevel%' NEQ '0' (
+    echo Requesting administrative privileges...
+    goto UACPrompt
+) else ( goto gotAdmin )
+
+:UACPrompt
+    echo Set UAC = CreateObject^("Shell.Application"^) > "%temp%\getadmin.vbs"
+    set params = %*:"=""
+    echo UAC.ShellExecute "cmd.exe", "/c %~s0 %params%", "", "runas", 1 >> "%temp%\getadmin.vbs"
+    "%temp%\getadmin.vbs"
+    del "%temp%\getadmin.vbs"
+    exit /B
+
+:gotAdmin
+    pushd "%cd%"
+    cd /d "%~dp0"
+taskkill /f /im wscript.exe
+taskkill /f /im mshta.exe
+taskkill /f /im cmd.exe
